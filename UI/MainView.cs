8a5582302d0873ad7b4841a80c0bc2a50c18f3ba
Andy Rodriguez <577147@student.inholland.nl>
2022-10-19 13:22:25 +0200

    Updated UI

--- UI/MainView.cs
+++ UI/MainView.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UI
 {
-    public partial class DashboardView : Form
+    public partial class MainView : Form
     {
-        public DashboardView()
+        public MainView()
         {
             InitializeComponent();
         }
