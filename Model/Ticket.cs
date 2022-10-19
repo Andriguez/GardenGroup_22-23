b0d41b0cf215ada6a1bdc0c7133eab266e3e0afe
Andy Rodriguez <577147@student.inholland.nl>
2022-10-19 12:42:06 +0200

    model updated

---
+++ Model/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Ticket
+    {
+        int Id { get; set; }
+        string TicketedBy { get; set; }
+        string ReportedBy { get; set; }
+        string Subject { get; set; }
+        DateTime date { get; set; }
+        string TicketType { get; set; }
+        string Priority { get; set; }
+        string Deadline { get; set; }
+        string Description { get; set; }
+    }
+}
